0f6f93c2f928d3d170215063a41728323b3d5b16
Matt Moore <mattmoor@google.com>
2019-03-09 22:37:57 +0000

    Add unary implementation

--- pkg/methods/sampleservice/unary/main.go
+++ pkg/methods/sampleservice/unary/main.go
@@ -2,13 +2,11 @@ package unary
 
 import (
 	"context"
-	"errors"
+	"fmt"
 
 	pb "github.com/mattmoor/korpc-sample/gen/proto"
 )
 
-
 func Impl(ctx context.Context, req *pb.Request) (*pb.Response, error) {
-	return nil, errors.New(`You need to implement SampleService.Unary!!!`)
+	return &pb.Response{Msg: fmt.Sprintf("%s - pong", req.Msg)}, nil
 }
-
